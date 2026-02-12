f14cc0c20df4e8f4958ed1bf79947402c0d24268
Flavio Costa <flavionegocios2004@gmail.com>
2026-02-12 16:25:57 -0300

    Adicionando funcionalidade de abrir e fechar modal, com useEffect, useState e useRef

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -1,9 +1,34 @@
-export function Dialog() {
+import { useEffect, useRef } from "react";
+import './dialog.style.css';
+
+export function Dialog( {isOpen , onClose}) {
+
+  const dialogRef = useRef(null);
+
+  const openDialog = () => {
+    dialogRef.current.showModal()
+  }
+
+  const closeDialog = () => {
+    dialogRef.current.close()
+  }
+
+  useEffect(() => {
+    console.log("Deveriamos mostrar o modal?", isOpen)
+    if (isOpen) {
+      openDialog()
+    } 
+    else {
+      closeDialog()
+    }
+  }, [isOpen])
+
+
   return (
     <>
-      <dialog>
-        <button></button>
-        <p></p>
+      <dialog ref={dialogRef}>
+        <button autoFocus onClick={onClose}>Fechar</button>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorum eaque aliquam quibusdam odio aut doloribus distincti</p>
       </dialog>
     </>
   );
